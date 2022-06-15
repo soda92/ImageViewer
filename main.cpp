38b5c258a36d2034d361f69d195f9cecf72eeafd
SodaCris <wang-jiaxun@outlook.com>
2022-06-15 17:51:00 +0800

    add target jump

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <QPushButton>
 #include <QScreen>
 #include <QScrollArea>
+#include <QScrollBar>
 
 class ImageScaleLabel : public QLabel {
   Q_OBJECT
@@ -26,11 +27,14 @@ public:
     this->setScaledContents(true);
   }
 
+  void setParentScrollArea(QScrollArea *parent_scroll_area) {
+    parent_scroll_area_ = parent_scroll_area;
+  }
+
 protected:
   void mousePressEvent(QMouseEvent *e) override final {
     if (e->button() == Qt::RightButton) {
-      selection_rect_ = {};
-      repaint();
+      clearSelection();
       if (selection_rect_.contains(e->pos()))
         context_menu_.exec(this->mapToGlobal(e->pos()));
     } else {
@@ -57,13 +61,59 @@ protected:
   }
   void mouseReleaseEvent(QMouseEvent *) override final {
     selection_started_ = false;
-    //    this->setPixmap(this->pixmap()->copy(selection_rect_));
+    qDebug() << "selected rectangle:" << selection_rect_;
+    zoomToRectangle(selection_rect_);
   }
+
+public slots:
+  void showImage(const QString &fileName) {
+    if (fileName != "") {
+      image_.load(fileName);
+      setPixmap(QPixmap::fromImage(image_));
+      zoomReset();
+    }
+  }
+
+  void zoomIn() { scaleImage(1.25); }
+
+  void zoomOut() { scaleImage(0.8); }
+
+  void zoomReset() { scaleImage(1 / scale_factor_); }
 
 private:
   bool selection_started_{false};
   QRect selection_rect_{};
   QMenu context_menu_{};
+  QImage image_{};
+  double scale_factor_{1.0};
+  QScrollArea *parent_scroll_area_{};
+
+  void clearSelection() {
+    selection_rect_ = {};
+    repaint();
+  }
+
+  void scaleImage(double scale_factor) {
+    scale_factor_ *= scale_factor;
+    qDebug() << "zoom level:" << scale_factor_;
+    resize(scale_factor_ * this->pixmap()->size());
+  }
+
+  void zoomToRectangle(QRect &target_rectangle) {
+    if (target_rectangle.x() == 0 || target_rectangle.y() == 0) {
+      return;
+    }
+    double required_factor =
+        1.0 * parent_scroll_area_->width() / target_rectangle.width();
+    scaleImage(required_factor);
+
+    parent_scroll_area_->horizontalScrollBar()->setValue(target_rectangle.x() *
+                                                         required_factor);
+    parent_scroll_area_->verticalScrollBar()->setValue(target_rectangle.y() *
+                                                       required_factor);
+
+    clearSelection();
+  }
 };
 
 class ImageViewer : public QMainWindow {
@@ -71,8 +121,12 @@ class ImageViewer : public QMainWindow {
 
 public:
   ImageViewer(QWidget *parent = nullptr) : QMainWindow(parent) {
-    showImage("D:/wallpaper-example.jpg");
+    setWindowTitle("图片查看");
+    image_scale_label_->showImage("D:/wallpaper-example.jpg");
+
     image_scroll_area_->setWidget(image_scale_label_);
+    image_scale_label_->setParentScrollArea(image_scroll_area_);
+    image_scroll_area_->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
     layout_main_->addWidget(image_scroll_area_);
 
     layout_buttons_->addWidget(select_file_button_);
@@ -89,11 +143,12 @@ public:
     connect(select_file_button_, &QPushButton::clicked, this,
             &ImageViewer::openFile);
 
-    connect(zoom_in_button_, &QPushButton::clicked, this, &ImageViewer::zoomIn);
-    connect(zoom_out_button_, &QPushButton::clicked, this,
-            &ImageViewer::zoomOut);
-    connect(zoom_reset_button_, &QPushButton::clicked, this,
-            &ImageViewer::zoomReset);
+    connect(zoom_in_button_, &QPushButton::clicked, image_scale_label_,
+            &ImageScaleLabel::zoomIn);
+    connect(zoom_out_button_, &QPushButton::clicked, image_scale_label_,
+            &ImageScaleLabel::zoomOut);
+    connect(zoom_reset_button_, &QPushButton::clicked, image_scale_label_,
+            &ImageScaleLabel::zoomReset);
 
     resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
   }
@@ -104,45 +159,23 @@ private:
 
   QHBoxLayout *layout_main_{new QHBoxLayout};
   QVBoxLayout *layout_buttons_{new QVBoxLayout};
-  QImage image_{};
+
   ImageScaleLabel *image_scale_label_{new ImageScaleLabel("图片区域")};
 
   QScrollArea *image_scroll_area_{new QScrollArea};
-  double scale_factor_{1.0};
 
   QPushButton *select_file_button_{new QPushButton("选择文件")};
   QPushButton *zoom_in_button_{new QPushButton("放大")};
   QPushButton *zoom_out_button_{new QPushButton("缩小")};
   QPushButton *zoom_reset_button_{new QPushButton("重置")};
 
-  void showImage(QString fileName) {
-    image_.load(fileName);
-    image_scale_label_->setPixmap(QPixmap::fromImage(this->image_));
-    zoomReset();
-  }
-
-  void scaleImage(double scale_factor) {
-    scale_factor_ *= scale_factor;
-    qDebug() << "zoom level:" << scale_factor_;
-    image_scale_label_->resize(scale_factor_ *
-                               image_scale_label_->pixmap()->size());
-  }
-
 private slots:
   void openFile() {
     QString fileName = QFileDialog::getOpenFileName(this, "打开文件", "D:/",
                                                     "Images (*.png *.jpg)");
     qDebug() << "file name:" << fileName;
-    if (fileName != "") {
-      showImage(fileName);
-    }
+    image_scale_label_->showImage(fileName);
   }
-
-  void zoomIn() { scaleImage(1.25); }
-
-  void zoomOut() { scaleImage(0.8); }
-
-  void zoomReset() { scaleImage(1 / scale_factor_); }
 };
 
 #include "main.moc"
