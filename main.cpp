4935e339e96b6515f38d192f33a07c893919eaea
SodaCris <wang-jiaxun@outlook.com>
2022-06-15 16:33:20 +0800

    add scroll area

--- main.cpp
+++ main.cpp
@@ -71,15 +71,18 @@ class ImageViewer : public QMainWindow {
 
 public:
   ImageViewer(QWidget *parent = nullptr) : QMainWindow(parent) {
-    layout_main_->addWidget(image_scale_label_);
+    showImage("D:/wallpaper-example.jpg");
+    image_scroll_area_->setWidget(image_scale_label_);
+    layout_main_->addWidget(image_scroll_area_);
 
     layout_buttons_->addWidget(select_file_button_);
     layout_buttons_->addWidget(zoom_in_button_);
     layout_buttons_->addWidget(zoom_out_button_);
     layout_buttons_->addWidget(zoom_reset_button_);
-    //    layout_buttons_->addWidget(new ImageScaleLabel);
+    buttons_widget_->setLayout(layout_buttons_);
+    buttons_widget_->setFixedWidth(200);
 
-    layout_main_->addLayout(layout_buttons_);
+    layout_main_->addWidget(buttons_widget_);
     main_widget_->setLayout(layout_main_);
     this->setCentralWidget(main_widget_);
 
@@ -92,18 +95,19 @@ public:
     connect(zoom_reset_button_, &QPushButton::clicked, this,
             &ImageViewer::zoomReset);
 
-    showImage("D:/wallpaper-example.jpg");
     resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
   }
 
 private:
   QWidget *main_widget_{new QWidget};
+  QWidget *buttons_widget_{new QWidget};
 
   QHBoxLayout *layout_main_{new QHBoxLayout};
   QVBoxLayout *layout_buttons_{new QVBoxLayout};
   QImage image_{};
   ImageScaleLabel *image_scale_label_{new ImageScaleLabel("图片区域")};
 
+  QScrollArea *image_scroll_area_{new QScrollArea};
   double scale_factor_{1.0};
 
   QPushButton *select_file_button_{new QPushButton("选择文件")};
@@ -112,12 +116,14 @@ private:
   QPushButton *zoom_reset_button_{new QPushButton("重置")};
 
   void showImage(QString fileName) {
-    this->image_.load(fileName);
-    this->image_scale_label_->setPixmap(QPixmap::fromImage(this->image_));
+    image_.load(fileName);
+    image_scale_label_->setPixmap(QPixmap::fromImage(this->image_));
+    zoomReset();
   }
 
   void scaleImage(double scale_factor) {
     scale_factor_ *= scale_factor;
+    qDebug() << "zoom level:" << scale_factor_;
     image_scale_label_->resize(scale_factor_ *
                                image_scale_label_->pixmap()->size());
   }
@@ -126,8 +132,10 @@ private slots:
   void openFile() {
     QString fileName = QFileDialog::getOpenFileName(this, "打开文件", "D:/",
                                                     "Images (*.png *.jpg)");
-    qDebug() << fileName;
-    showImage(fileName);
+    qDebug() << "file name:" << fileName;
+    if (fileName != "") {
+      showImage(fileName);
+    }
   }
 
   void zoomIn() { scaleImage(1.25); }
